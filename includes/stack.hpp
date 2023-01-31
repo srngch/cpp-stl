950ce0117e2c5b97d25ca869ea11f47993b4c59f
srngch <srngch.dev@gmail.com>
2023-01-31 15:54:34 +0900

    Change default container of stack(std to ft)

--- includes/stack.hpp
+++ includes/stack.hpp
@@ -1,12 +1,12 @@
 #ifndef __STACK_HPP__
 #define __STACK_HPP__
 
-#include <vector>
+#include "vector.hpp"
 
 namespace ft
 {
 
-template <class T, class Container = std::vector<T> >
+template <class T, class Container = ft::vector<T> >
 class stack
 {
 public:
