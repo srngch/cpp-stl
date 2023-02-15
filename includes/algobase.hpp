b0dfaa5b5e5e2ff9c5453406e1557dc2dd908c14
srngch <srngch.dev@gmail.com>
2023-02-15 13:28:07 +0900

    Tidy up comment

--- includes/algobase.hpp
+++ includes/algobase.hpp
@@ -19,30 +19,30 @@ inline void swap(Tp& a, Tp& b) {
 template <typename InputIterator1, typename InputIterator2>
 inline bool equal(InputIterator1 first1, InputIterator1 last1,
                   InputIterator2 first2) {
-  for (; first1 != last1; ++first1, ++first2) {
+  while (first1 != last1) {
     if (!(*first1 == *first2))
       return false;
+    ++first1;
+    ++first2;
   }
   return true;
 }
 
 /**
  * @brief Performs "dictionary" comparison on ranges.
- *
- * Returns true if the sequence of elements defined by the range
- * [first1,last1) is lexicographically less than the sequence of elements
- * defined by the range [first2,last2). Returns false otherwise.
  */
 template <typename InputIterator1, typename InputIterator2>
 bool lexicographical_compare(InputIterator1 first1,
                              InputIterator1 last1,
                              InputIterator2 first2,
                              InputIterator2 last2) {
-  for (; first1 != last1 && first2 != last2; ++first1, ++first2) {
+  while (first1 != last1 && first2 != last2) {
     if (*first1 < *first2)
       return true;
     if (*first2 < *first1)
       return false;
+    ++first1;
+    ++first2;
   }
   return first1 == last1 && first2 != last2;
 }
